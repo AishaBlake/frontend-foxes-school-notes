07375e52152f33788d0fe1af8d1fe79c99d855e6
Aisha Blake <goaliegirlp@aol.com>
2021-05-26 21:08:34 -0400

    Add forms to bank app

--- bank-app/app.js
+++ bank-app/app.js
@@ -36,3 +36,31 @@ function onLinkClick(event) {
 
 window.onpopstate = () => updateRoute();
 updateRoute();
+
+async function register() {
+  const registerForm = document.getElementById('registerForm');
+  const formData = new FormData(registerForm);
+  const data = Object.fromEntries(formData);
+  const jsonData = JSON.stringify(data);
+
+  const result = await createAccount(jsonData);
+
+  if (result.error) {
+    return console.log('An error occured:', result.error);
+  }
+
+  console.log('Account created!', result);
+}
+
+async function createAccount(account) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: account
+    });
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
