996298246a0bd2dbe35b705771fff1b154d8179f
Aisha Blake <goaliegirlp@aol.com>
2021-05-31 21:00:23 -0400

    Use bank data

--- bank-app/app.js
+++ bank-app/app.js
@@ -7,6 +7,14 @@ const routes = {
   },
 };
 
+let account = null;
+
+function updateElement(id, textOrNode) {
+  const element = document.getElementById(id);
+  element.textContent = '';
+  element.append(textOrNode);
+}
+
 function updateRoute() {
   const path = window.location.pathname;
   const route = routes[path];
@@ -37,6 +45,29 @@ function onLinkClick(event) {
 window.onpopstate = () => updateRoute();
 updateRoute();
 
+async function login() {
+  const loginForm = document.getElementById('loginForm')
+  const user = loginForm.user.value;
+  const data = await getAccount(user);
+
+  if (data.error) {
+    return updateElement('loginError', data.error);
+  }
+
+  account = data;
+  navigate('/dashboard');
+  updateDashboard();
+}
+
+async function getAccount(user) {
+  try {
+    const response = await fetch('//localhost:5000/api/accounts/' + encodeURIComponent(user));
+    return await response.json();
+  } catch (error) {
+    return { error: error.message || 'Unknown error' };
+  }
+}
+
 async function register() {
   const registerForm = document.getElementById('registerForm');
   const formData = new FormData(registerForm);
@@ -50,6 +81,8 @@ async function register() {
   }
 
   console.log('Account created!', result);
+  navigate('/dashboard');
+  updateDashboard();
 }
 
 async function createAccount(account) {
@@ -63,4 +96,31 @@ async function createAccount(account) {
   } catch (error) {
     return { error: error.message || 'Unknown error' };
   }
+}
+
+function updateDashboard() {
+  if (!account) {
+    return navigate('/login');
+  }
+
+  updateElement('description', account.description);
+  updateElement('balance', account.balance.toFixed(2));
+  updateElement('currency', account.currency);
+
+  const transactionsRows = document.createDocumentFragment();
+  for (const transaction of account.transactions) {
+    const transactionRow = createTransactionRow(transaction);
+    transactionsRows.appendChild(transactionRow);
+  }
+  updateElement('transactions', transactionsRows);
+}
+
+function createTransactionRow(transaction) {
+  const template = document.getElementById('transaction');
+  const transactionRow = template.content.cloneNode(true);
+  const tr = transactionRow.querySelector('tr');
+  tr.children[0].textContent = transaction.date;
+  tr.children[1].textContent = transaction.object;
+  tr.children[2].textContent = transaction.amount.toFixed(2);
+  return transactionRow;
 }
